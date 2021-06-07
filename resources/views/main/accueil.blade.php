66cc944d141c78c01f8a9ccd7baf3a9303a87ccc
Zwydge <zwedge.message@gmail.com>
2021-06-07 15:03:17 +0200

    Search bar

--- resources/views/main/accueil.blade.php
+++ resources/views/main/accueil.blade.php
@@ -5,6 +5,9 @@
         <div class="title m-b-md">
             Knowledger
         </div>
+        <div class="container_search_bar">
+            <input placeholder="Posez votre question" class="search_bar" type="text">
+        </div>
     </div>
 @endsection
 
