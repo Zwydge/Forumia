0d0294c1106730748ee345b3d5e7d8d2adfd74b7
DaysukAM <anthony.moutonnet@gmail.com>
2022-02-21 10:34:47 +0100

    usercontrolleur update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,8 +18,6 @@ class UserController extends Controller
         $user = User::select()->where('id', Auth::user()->id)->first();
         return view('main/account', ['role' => $role->name], ['name' => $user->name]);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     }
 
     public function update(Request $request)
@@ -47,57 +45,11 @@ class UserController extends Controller
 
         $user->save();
 
-<<<<<<< HEAD
-            $role = Roles::select()->where('id', Auth::user()->roles_id)->first();
-
-            return view('main/account', ['role' => $role -> name]);
-    }
-
-=======
-    }
-
-=======
-    }
-
->>>>>>> 543d51d (account management page creation and data update)
-        public function update(Request $request)
-        {
-            $input = $request->all();
-            //$avatar = $request->file('avatar');
-            $user = User::select()->where('id', Auth::user()->id)->first();
-            if(isset($input['username'])){
-                $user->name = $input['username'];
-            }
-            if(isset($input['email'])){
-                $user->password = bcrypt($input['email']);
-            }
-            //if(isset($input['password'])){
-            //    $user->password = bcrypt($input['password']);
-            //}
-            //$file = $request->avatar;
-            //if(isset($avatar)){
-            //    $location = "public/media/avatar/".Auth::user()->id."";
-            //    File::deleteDirectory(public_path("/media/avatar/".Auth::user()->id));
-            //    $avatar->move(base_path($location), $file->getClientOriginalName());
-             //   $user->avatar = "".Auth::user()->id."/".$file->getClientOriginalName();
-            //}
-            $user->save();
-=======
         $role = Roles::select()->where('id', Auth::user()->roles_id)->first();
-<<<<<<< HEAD
->>>>>>> 53c4cfb ( minor fix)
 
-        return Response::json(['avatar' => $avatar, 'user' => $user], 200);
-        return view('main/account', ['role' => $role->name]);
-=======
-      
         //return Response::json(['avatar' => $avatar, 'user' => $user], 200);
         return view('main/account', ['role' => $role->name], ['name' => $user->name]);
->>>>>>> 8a6b01d (Correction actualisation bug)
     }
 
-<<<<<<< HEAD
->>>>>>> 8920e49 (account management page creation and data update)
-=======
->>>>>>> 543d51d (account management page creation and data update)
+
 }
